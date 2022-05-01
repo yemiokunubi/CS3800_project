12a092c9d022bcdf0cbda16c62f9ce03d7021222
Christopher Hoang <christopherhoang707@gmail.com>
2022-04-30 17:00:48 -0700

    Updated to work with GUI

--- MailClient.java
+++ MailClient.java
@@ -8,7 +8,7 @@
  *
  */
 
-import java.util.Properties;
+import java.util.Properties; 
 import java.util.Scanner;
 
 import javax.activation.*;
@@ -17,13 +17,10 @@ import javax.mail.internet.*;
 
 public class MailClient
 {
-	public static void main(String[] args) 
+	public static void sendMail(String recepient, String subject, String body, String filePath) 
 	{
-		// Ask for recepient's email address.
-		Scanner scnr = new Scanner(System.in);
-		System.out.println("What is the recepient's email?");
-		String to = scnr.nextLine();
 
+		String to = recepient;
 		// Declare sender's email address and information.
 		String from = "billybronconetworking2022@gmail.com";
 
@@ -41,6 +38,7 @@ public class MailClient
 		properties.put("mail.smtp.port", "587");
 		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 		properties.put("mail.smtp.ssl.protocols", "TLSv1.2");
+		properties.put("mail.smtp.ssl.checkserveridentity", "true");
 
 		// Get the Session object.
 		Session session = Session.getInstance(properties, new javax.mail.Authenticator() 
@@ -63,31 +61,14 @@ public class MailClient
 			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
 
 			// Set Subject: header field.
-			System.out.println("Type subject text: ");
-			message.setSubject(scnr.nextLine());
+			
+			message.setSubject(subject);
 
 			// Create the message body object.
 			BodyPart messageBodyPart = new MimeBodyPart();
 
-			// Ask user to compose message body.
-			System.out.println("Type body text (type 'done' to finish): ");
-			String bodyText;
-			String resultBodyText = "";
 
-			// Declare value for infinite while loop
-			int inf = 0;
-			
-			// Scan all input until 'done' is read.
-			while (inf != 1) 
-			{
-				bodyText = scnr.nextLine();
-				if (bodyText.contains("done")) {
-					break;
-				}
-				resultBodyText += bodyText + "\n";
-			}
-
-			messageBodyPart.setText(resultBodyText + "\n");
+			messageBodyPart.setText(body + "\n");
 
 			// Create a multipart message.
 			Multipart multipart = new MimeMultipart();
@@ -99,29 +80,24 @@ public class MailClient
 			messageBodyPart = new MimeBodyPart();
 			
 			// Ask user for file.
-			System.out.println("Attach a file (give file path, or type 'none' for no attachment): ");
 			// EX - /Users/kyle/eclipse-workspace/MailClient/bin/filename (on Apple, will be different on Windows)
-			String fileData = scnr.nextLine();
-			if (!(fileData.contains("none"))) 
+			String fileData = filePath;
+			if(fileData != null)
 			{
 				DataSource source = new FileDataSource(fileData);
-			
-				// Ask user for attachment name.
-				System.out.println("Set name for your attachment: ");
-				String fileName = scnr.nextLine();
 				
 				messageBodyPart.setDataHandler(new DataHandler(source));
-				messageBodyPart.setFileName(fileName);
+				messageBodyPart.setFileName(fileData);
 				multipart.addBodyPart(messageBodyPart);
 			}
+			
 
 			// Send the complete message parts.
 			message.setContent(multipart);
-
+			
 			// Send message.
 			Transport.send(message);
 
-			System.out.println("Sent message successfully....");
 		}
 
 		catch (MessagingException e) 
