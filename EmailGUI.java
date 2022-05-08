20b6e452140d5bc89dc98ea01f41dd2a56950cf9
Christopher Hoang <christopherhoang707@gmail.com>
2022-05-07 23:15:00 -0700

    GUI for inbox

---
+++ EmailGUI.java
@@ -0,0 +1,109 @@
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.DefaultListModel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JList;
+
+public class EmailGUI extends JFrame {
+
+	private JPanel contentPane;
+	private ArrayList<String> emails = FetchMail.checkMail("pop.gmail.com", "pop3", "billybronconetworking2022@gmail.com", "cs380001");
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EmailGUI frame = new EmailGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public EmailGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 675, 450);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JButton btnNewButton = new JButton("+ Compose");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JScrollPane scrollPane = new JScrollPane();
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap(139, Short.MAX_VALUE)
+					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 133, GroupLayout.PREFERRED_SIZE))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(131)
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 385, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(135, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(14)
+					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 336, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		
+		
+		DefaultListModel listModel = new DefaultListModel();
+		for(int i = 0; i < emails.size(); i++)
+		{
+			listModel.addElement(emails.get(i));
+		}
+		
+		JList list = new JList(listModel);
+		list.setCellRenderer(new MyListCellRenderer());
+		scrollPane.setViewportView(list);
+		contentPane.setLayout(gl_contentPane);
+	}
+	
+	
+	//Helper class for the JList. Normally, the JList will show items on a single line; this class helps break up each item
+	//while making the entire thing still selectable in the JList
+	private class MyListCellRenderer extends DefaultListCellRenderer 
+	{
+        @Override
+        public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) 
+        {
+            super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+           
+            String labelText = emails.get(index);
+            setText(labelText);
+
+            return this;
+        }
+
+    }
+}
